ea2eb83c75db6118c509ba9350bf1c647052b5ee
Emiliano Guttlein <emiliano.guttlein@claro.com.ar>
2024-10-24 10:49:01 -0300

    added FlatList and refactor some code

--- app/RefundsScreen.tsx
+++ app/RefundsScreen.tsx
@@ -1,11 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function RefundsScreen() {
   return (
-    <View>
-      {/* Tab nav */}
-      <Text>Chau</Text>
-      {/* Content */}
+    <View style={styles.container}>
+      <Text style={styles.container__pageTitle}>Mis Devoluciones</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  container__pageTitle: {
+    marginVertical: 20,
+    alignSelf: "flex-start",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#555555",
+  },
+});
